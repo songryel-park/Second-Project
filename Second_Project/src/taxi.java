fd5829b15f76e5a82819d617da5f7681726e3b92
songryel-park <fuf3413@gmail.com>
2022-05-18 19:09:13 +0900

    최종본

--- Second_Project/src/taxi.java
+++ Second_Project/src/taxi.java
@@ -45,7 +45,7 @@ public class taxi {
 		if(state.equals("empty")) {
 			this.pas = pas;
 			if(pas > 0) {
-				this.state = "drive";
+				this.state = "목적지";
 				System.out.println("운행중");
 			} else {
 				System.out.println("빈차");
@@ -58,16 +58,16 @@ public class taxi {
 	public void go(String station) {
 		if(station == "S") {
 			this.station = "서울역";
-			amount = 100;
+			amount = 3;
 		} else if(station == "H") {
 			this.station = "홍대";
-			amount = 150;
+			amount = 12;
 		} else if(station == "I") {
 			this.station = "인천";
-			amount = 400;
+			amount = 65;
 		} else if(station == "B") {
 			this.station = "부산";
-			amount = 1000;
+			amount = 400;
 		}
 	}
 	
@@ -83,16 +83,16 @@ public class taxi {
 	
 	public void pay() {		
 		this.amount = amount;
-		if (amount <= 100) {
+		if (amount <= 10) {
 	        price = 3000;
-	    } else if (amount > 100 && amount <= 300) {
-            addPrice = 10 * (amount-100);
+	    } else if (amount > 10 && amount <= 50) {
+            addPrice = 50 * (amount-10);
 	    	price = 3000;
-	    } else if (amount > 300 && amount <= 500) {
-            addPrice = (10 * 100) + (30 * 200) + (50 * (amount-300));
+	    } else if (amount > 50 && amount <= 100) {
+            addPrice = (50 * 40) + (200 * (amount-50));
 	    	price = 3000;
 	    } else {
-	    	addPrice = (10 * 100) + (30 * 200) + (50 * 200) + (100 * (amount-500));
+	    	addPrice = (50 * 40) + (200 * 50) + (1000 * (amount-100));
 	    	price = 3000;
 	    }
 		total = (price + addPrice);
@@ -100,15 +100,15 @@ public class taxi {
 	
 	public static void main(String[] args) {	
 		taxi t1 = new taxi(1);
-		t1.start(11);
+		t1.start(33);
 		t1.take(7);
 		t1.go("I");
-		t1.convert(5);
+		t1.convert(10);
 		t1.convert(5);
 		t1.convert(-2);
 		t1.pay();
 		
-		System.out.println(t1.num + " " + t1.oil + " " + t1.pas + " " + t1.state + " " + t1.station + " " 
-				+ t1.speed + " " + t1.amount + " " + t1.price + " " + t1.addPrice + " " + t1.total + " ");
+		System.out.println(t1.num + "번 택시/" + t1.oil + "L/" + t1.pas + "명 탑승/" + t1.state + " " + t1.station + "까지"  
+					+ " 거리 " + t1.amount + "km/" + "기본금 " + t1.price + "원" + " 추가 금액 " + t1.addPrice + "원" + " 총 결제액 " + t1.total + "원");
 	}
 }
