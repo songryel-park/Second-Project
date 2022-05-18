93e032df502326d65be1d533b72500a3a2109f83
songryel-park <105186195+songryel-park@users.noreply.github.com>
2022-05-18 21:03:52 +0900

    Update bus.java

--- Second_Project/src/bus.java
+++ Second_Project/src/bus.java
@@ -21,11 +21,11 @@ public class bus {
 		this.price = price;
 	}
 	
-	public bus(int num) {
+	public bus(int num) {							// 버스번호 및 초기상태화
 		this(num, "운행", 0, 25, 100, 0, 0);
 	}
 	
-	public void fuel(int oil) {
+	public void fuel(int oil) {						// 주유량에 따른 차량 상태
 		this.state = state;
 		if(oil < 10) {
 			this.oil = oil;
@@ -38,7 +38,7 @@ public class bus {
 		}
 	}
 	
-	public void convert(int speed) {
+	public void convert(int speed) {					// 변속
 		if(speed >= 0) {
 			this.speed += speed;
 			//System.out.println("빠르게");
@@ -48,7 +48,7 @@ public class bus {
 		}
 	}
 	
-	public void runnig(int pas) {
+	public void runnig(int pas) {						// 인원수에 따른 차량 상태
 		if(pas >= maxPas) {
 			System.out.println("만차");
 		} else if(pas < 0) {
@@ -59,7 +59,7 @@ public class bus {
 		}
 	}
 	
-	public void pay(int age) {
+	public void pay(int age) {						// 나이별 버스 요금
 		if(age < 5) {
 			price = 0;
 		} else if(age >= 5 && age < 12) {
@@ -72,14 +72,14 @@ public class bus {
 	}
 	
 	public static void main(String[] args) {
-		bus b1 = new bus(1); // 버스번호 기입
-		b1.fuel(31); // 주유량 기입
+		bus b1 = new bus(1); 						// 버스번호 기입
+		b1.fuel(31); 							// 주유량 기입
 		b1.runnig(7); 
-		b1.runnig(11); // 추가할 수록 인원수가 변화함
+		b1.runnig(11); 							// 추가할 수록 인원수가 변화함
 		b1.convert(30);
 		b1.convert(10);
-		b1.convert(-5); // 추가할 수록 속도가 변함
-		b1.pay(19); // 나이에 따라 요금이 
+		b1.convert(-5); 						// 추가할 수록 속도가 변함
+		b1.pay(19); 							// 나이에 따른 요금
 		
 		System.out.println(b1.num + "번 버스 " + "잔여 주유량 " + b1.oil + "L " + "현재 " + b1.pas + "명 탑승 중 "
 				+ "현재 속도 "+ b1.speed + "km/h " + "요금 " + b1.price + "원");
