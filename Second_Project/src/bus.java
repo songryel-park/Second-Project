f7c6fac94a0fde516aa9764f9bf2b321e8b006a7
songryel-park <105186195+songryel-park@users.noreply.github.com>
2022-05-18 20:55:07 +0900

    Update bus.java

--- Second_Project/src/bus.java
+++ Second_Project/src/bus.java
@@ -72,14 +72,14 @@ public class bus {
 	}
 	
 	public static void main(String[] args) {
-		bus b1 = new bus(1);
-		b1.fuel(31);
-		b1.runnig(7);
-		b1.runnig(11);
+		bus b1 = new bus(1); // 버스번호 기입
+		b1.fuel(31); // 주유량 기입
+		b1.runnig(7); 
+		b1.runnig(11); // 추가할 수록 인원수가 변화함
 		b1.convert(30);
 		b1.convert(10);
-		b1.convert(-5);
-		b1.pay(19);
+		b1.convert(-5); // 추가할 수록 속도가 변함
+		b1.pay(19); // 나이에 따라 요금이 
 		
 		System.out.println(b1.num + "번 버스 " + "잔여 주유량 " + b1.oil + "L " + "현재 " + b1.pas + "명 탑승 중 "
 				+ "현재 속도 "+ b1.speed + "km/h " + "요금 " + b1.price + "원");
